d980c18d99cdae896fb517e9505b3b8b2a8a28c6
Prrrf <prrrffrrrp@gmail.com>
2017-11-18 08:01:55 +0100

    Move exceptions to its own file

---
+++ remaner/exceptions.py
@@ -0,0 +1,26 @@
+class PathDoesNotExistError(Exception):
+    pass
+
+
+class EmptyDirectoryError(Exception):
+    pass
+
+
+class DirectoryNotFoundError(Exception):
+    pass
+
+
+class FileDoesNotExistError(Exception):
+    pass
+
+
+class FileExtensionNotSupported(Exception):
+    pass
+
+
+class IndexOutOfRangeError(Exception):
+    pass
+
+
+class NotAValidOption(Exception):
+    pass
--- remaner/renamer.py
+++ remaner/renamer.py
@@ -1,38 +1,9 @@
 import os
 import textract
 from itertools import zip_longest
-
-
-class PathDoesNotExistError(Exception):
-    pass
-
-
-class EmptyDirectoryError(Exception):
-    pass
-
-
-class DirectoryNotFoundError(Exception):
-    pass
-
-
-class FileDoesNotExistError(Exception):
-    pass
-
-
-class FileExtensionNotSupported(Exception):
-    pass
-
-
-class NotSameLenghtError(Exception):
-    pass
-
-
-class IndexOutOfRangeError(Exception):
-    pass
-
-
-class NotAValidOption(Exception):
-    pass
+from exceptions import PathDoesNotExistError, EmptyDirectoryError,\
+    DirectoryNotFoundError, FileDoesNotExistError, FileExtensionNotSupported,\
+    IndexOutOfRangeError, NotAValidOption
 
 
 class InputCheckExtract:
