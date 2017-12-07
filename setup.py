76f0289a9c92abc1063d5b00b0a414ae6cdf36c1
Prrrf <prrrffrrrp@gmail.com>
2017-12-07 06:51:58 +0100

    Create a package and install it

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='remaner',
       version='0.1',
       description='File renamer where the new names are supplied from a file.',
+      long_description=long_description,
       url='https://github.com/prrrffrrrp/RemaneR',
       author='prrrf',
       author_email='prrrffrrrp@gmail.com',
       license='MIT',
       keywords='rename renamefiles renamefromfile cool cli notboring',
       packages=find_packages(exclude=['tests']),
-      install_requires =['textract', 'docopt', 'colorama'],
+      install_requires=['textract', 'docopt', 'colorama'],
       python_requires='>=3',
+      scripts=['bin/remaner'],
       zip_safe=False)
